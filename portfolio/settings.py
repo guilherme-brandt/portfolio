e603ff3764839d2fec9e60953d568983af2a7161
guilherme-brandt <glbfaria42@gmail.com>
2024-10-04 11:15:51 -0300

    deleted styles.css

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -102,8 +102,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+# # Database
+# # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 import dj_database_url
 # Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
@@ -113,7 +113,12 @@ DATABASES = {
         conn_max_age=600
     )
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
